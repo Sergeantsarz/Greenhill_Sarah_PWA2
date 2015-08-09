5733319bc089efb16f69352ba5b72b8ed067382d
Sarah Greenhill <sarah@sarahgreenhill.com>
2015-08-09 16:50:14 -0400

    Adding to Docs
    Added target areas to HTML to remember what to target with jQuery

--- Greenhill_Saraj_Projectwk1/main.js
+++ Greenhill_Saraj_Projectwk1/main.js
@@ -1,6 +1,13 @@
 /*
-Description: DESCRIPTION INFO GOES HERE
-Author: Name
-Date: DATE GOES HERE
+Description: PWA2 - Week One - Project One
+Author: Sarah R. Greenhill - 770562
+Date: August 9, 2015
 */
+
+$(document).ready(function() {
+	$("#contentleft ul").css("backgroundColor", "orange");
+	
+	console.log($("#contentleft ul").offset());
+	console.log($("#contentleft ul").position());
+});
 
